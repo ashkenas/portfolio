767d6c429c095463aacb8c60b79596aefc89bf98
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-08-27 20:45:35 -0400

    Fixed disappearing page on double navigate

--- src/components/AnimatedLink.js
+++ src/components/AnimatedLink.js
@@ -1,10 +1,11 @@
 import { useCallback, useContext } from "react";
-import { Link, useHref, useLinkClickHandler } from "react-router-dom";
+import { Link, useHref, useLinkClickHandler, useLocation } from "react-router-dom";
 import { NavContext } from "../NavContext";
 
 export default function AnimatedLink({ to, replace, state, target, ...passThrough }) {
   const [tId, setTId] = useContext(NavContext);
   const href = useHref(to);
+  const { pathname } = useLocation();
   const doNavigation = useLinkClickHandler(to, {
     replace,
     state,
@@ -13,10 +14,14 @@ export default function AnimatedLink({ to, replace, state, target, ...passThroug
 
   const clickHandler = useCallback((e) => {
     e.preventDefault();
+    if (pathname === href) return;
     clearTimeout(tId);
     document.querySelector('main')?.classList.add('hide');
-    setTId(setTimeout(() => doNavigation(e), 250));
-  }, [doNavigation, tId, setTId]);
+    setTId(setTimeout(() => {
+      document.querySelector('main')?.classList.remove('hide');
+      doNavigation(e)
+    }, 250));
+  }, [doNavigation, tId, setTId, pathname]);
 
   return (
     <Link {...passThrough} to={href}
